e5e8046cae90bfb34eddaca3896a910af8c325a3
Steven Blom <steven.blom@drawboard.com>
2025-05-11 23:11:51 +0930

    Update included SDKs

--- InstallVS.cmd
+++ InstallVS.cmd
@@ -8,8 +8,11 @@ call C:\Build\CollectLogs.cmd C:\Build\vs_buildtools.exe --wait --quiet --norest
     --add Microsoft.NetCore.Component.Runtime.6.0 ^
     --add Microsoft.NetCore.Component.Runtime.7.0 ^
     --add Microsoft.NetCore.Component.Runtime.8.0 ^
+    --add Microsoft.NetCore.Component.Runtime.9.0 ^
     --add Microsoft.VisualStudio.Component.Windows10SDK.18362 ^
     --add Microsoft.VisualStudio.Component.Windows10SDK.19041 ^
+    --add Microsoft.VisualStudio.Component.Windows11SDK.22621 ^
+    --add Microsoft.VisualStudio.Component.Windows11SDK.26100 ^
     --remove Microsoft.VisualStudio.Component.Windows10SDK.10240 ^
     --remove Microsoft.VisualStudio.Component.Windows10SDK.10586 ^
     --remove Microsoft.VisualStudio.Component.Windows10SDK.14393 ^
