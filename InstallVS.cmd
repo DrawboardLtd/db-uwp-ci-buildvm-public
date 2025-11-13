c74d4f164975ff27119112ee619aafe5afa53dc9
Steven Blom <steven.blom@drawboard.com>
2025-11-13 16:45:30 +1030

    Improve logged text

--- InstallVS.cmd
+++ InstallVS.cmd
@@ -1,5 +1,5 @@
 @if not defined _echo echo off
-echo "Installing Visual Studio Build Tools 2022"
+echo Installing Visual Studio Build Tools 2022
 call C:\Build\CollectLogs.cmd C:\Build\2022\vs_buildtools.exe --wait --quiet --norestart --nocache ^
     --installPath "%ProgramFiles%\Microsoft Visual Studio\2022\BuildTools" ^
     --add Microsoft.VisualStudio.Workload.MSBuildTools ^
@@ -13,7 +13,7 @@ call C:\Build\CollectLogs.cmd C:\Build\2022\vs_buildtools.exe --wait --quiet --n
     --add Microsoft.VisualStudio.Component.Windows11SDK.26100 ^
     || IF "%ERRORLEVEL%"=="3010" EXIT 0
 
-echo "Installing Visual Studio Build Tools 2026"
+echo Installing Visual Studio Build Tools 2026
 call C:\Build\CollectLogs.cmd C:\Build\2026\vs_buildtools.exe --wait --quiet --norestart --nocache ^
     --installPath "%ProgramFiles%\Microsoft Visual Studio\2026\BuildTools" ^
     --add Microsoft.VisualStudio.Workload.MSBuildTools ^
@@ -28,7 +28,7 @@ call C:\Build\CollectLogs.cmd C:\Build\2026\vs_buildtools.exe --wait --quiet --n
     --add Microsoft.VisualStudio.Component.Windows11SDK.26100 ^
     || IF "%ERRORLEVEL%"=="3010" EXIT 0
 
-echo "Installing Microsoft Store Services SDK"
+echo Installing Microsoft Store Services SDK
 msiexec /i C:\Build\MicrosoftStoreServicesSDK.msi /quiet
 
-echo "Visual Studio Build Tools installation complete"
+echo Visual Studio Build Tools installation complete
