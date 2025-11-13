d028cfd253a61ea1931fc0f35ff8cc9b74c02222
Steven Blom <steven.blom@drawboard.com>
2025-11-13 16:10:30 +1030

    Download VS 2026

--- BuildBaseImage.ps1
+++ BuildBaseImage.ps1
@@ -8,11 +8,18 @@ if (-not (Test-Path "C:\Build")) {
     New-Item -Path "c:\" -Name "Build" -ItemType Directory | Out-Null
 }
 Push-Location C:\Build
+New-Item -Path "c:\Build" -Name "2022" -ItemType Directory | Out-Null
+New-Item -Path "c:\Build" -Name "2026" -ItemType Directory | Out-Null
 Invoke-WebRequest https://aka.ms/vscollect.exe -OutFile C:\Build\collect.exe
-Invoke-WebRequest https://aka.ms/vs/17/release/vs_buildtools.exe -OutFile C:\Build\vs_buildtools.exe
-Invoke-WebRequest https://aka.ms/vs/17/release/channel -OutFile C:\Build\VisualStudio.chman
+
+Invoke-WebRequest https://aka.ms/vs/17/release/vs_buildtools.exe -OutFile C:\Build\2022\vs_buildtools.exe
+#Invoke-WebRequest https://aka.ms/vs/17/release/channel -OutFile C:\Build\2022\VisualStudio.chman
+
+Invoke-WebRequest https://download.visualstudio.microsoft.com/download/pr/d3b4e0f6-4bc0-4ec0-ba9c-20b355d61cc4/c3e4b70501bd09103bd52cf66d331dd29c80700fa63a2f8a872e365ff5a4282e/vs_BuildTools.exe -OutFile C:\Build\2026\vs_buildtools.exe
+
 Invoke-WebRequest "$ScriptSourceRepo/CollectLogs.cmd" -OutFile C:\Build\CollectLogs.cmd
 Invoke-WebRequest "$ScriptSourceRepo/InstallVS.cmd" -OutFile C:\Build\InstallVS.cmd
+
 Invoke-WebRequest https://marketplace.visualstudio.com/_apis/public/gallery/publishers/AdMediator/vsextensions/MicrosoftStoreServicesSDK/10.0.5/vspackage -OutFile C:\Build\MicrosoftStoreServicesSDK.msi
 
 "Configuring Virtual Machine"
